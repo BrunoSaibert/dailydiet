5762ec5cc459626fe5d02ae5cbf7bcbf5bcc0ebc
Bruno Saibert <brunosaibert@gmail.com>
2022-12-16 17:17:44 -0300

    feat: add styled-components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,17 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
+
+import * as S from "./styles";
 
 export function Dashboard() {
   return (
-    <View style={styles.container}>
+    <S.Container>
       <Text>Dashboard</Text>
-    </View>
+    </S.Container>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
